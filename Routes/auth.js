2f199b7c484e31a717831c725b8482d3038fdea4
Ankit Kumar <ak8068716@gmail.com>
2022-10-12 17:51:28 +0530

    JWT Token

--- Routes/auth.js
+++ Routes/auth.js
@@ -2,11 +2,12 @@ const express = require('express')
 const routes = express.Router()
 const { signUp,signIn } = require('../Controller/auth')
 const {checkDuplicateUsernameAndEmail,checkRoles} = require('../middleware/user')
+const {verifyToken} = require('../middleware/AuthJwt')
 
  routes.post('/api/books/v1/signup',[checkDuplicateUsernameAndEmail],[checkRoles],
 signUp)
 
 
-routes.post('/api/books/v1/signIn',signIn)
+routes.post('/api/books/v1/signIn',[verifyToken],signIn)
 
 module.exports = {authRoutes : routes}
